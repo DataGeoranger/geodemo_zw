38270cc759a8b9cf76f329709b866f7129fcd88b
Jack Zhaowei Wang <zhaowei.wang1987@gmail.com>
2021-03-14 22:01:02 -0400

    Init commit

---
+++ tests/test_geodemo_zw.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `geodemo_zw` package."""
+
+
+import unittest
+
+from geodemo_zw import geodemo_zw
+
+
+class TestGeodemo_zw(unittest.TestCase):
+    """Tests for `geodemo_zw` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
